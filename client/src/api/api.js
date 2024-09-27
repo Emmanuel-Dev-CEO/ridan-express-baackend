bd26de8f4f072d8b340a6bbc55b1f649295dc954
Emmanuel Dev <emmanueldev584@gmail.com>
2024-09-26 17:48:40 -0700

    updated

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const local = 'http://localhost:5000'
-const production = 'http://localhost:6000'
+const production = 'https://ridanexpress-marketplace-backend.vercel.app/'
 
 let api_url = ''
 let mode = 'pro'
